f7907153d793a303de409dd11c0bf03a4b629e86
Karl Kraft <karl@kraft.com>
2023-12-29 23:24:21 -0600

    Cleanup after testing on UNO R4

--- ArduinoDriver/ArduinoDriver.ino
+++ ArduinoDriver/ArduinoDriver.ino
@@ -7,9 +7,9 @@
 //
 
 #include "secrets.h"
+#include <SharedSecrets.h>
 #include <WiFiS3.h>
 #include <FastLED.h>
-
 //
 // WIFI
 //
@@ -34,49 +34,74 @@ typedef struct {
   int8_t red;
   int8_t green;
   int8_t blue;
+  int8_t padding;
 } Packet;
 
-#define PACKET_MAGIC 0xda1c
+#define PACKET_MAGIC 0x1234
 
 Packet packetBuffer;
 
 //
 //  LEDs
 //
-#define NUM_LEDS 9
-#define LED_PIN 5
+#define NUM_LEDS 12
+#define LED_PIN 6
 CRGB leds[NUM_LEDS];
 
 
 void setup() {
+
+  // Configure LEDs
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(255);
+  for (int x = 0; x < NUM_LEDS; x++) {
+    leds[x] = CRGB::Black;
+  }
+  FastLED.show();
+
+
   // wait for serial port
   Serial.begin(9600);
   while (!Serial) {
     ;
   }
 
-  // Configured LEDs
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(25);
+  delay(1000);
+
+
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("WiFi module failed (WL_NO_MODULE)");
+    Serial.println(" >> HALTED <<");
     while (true)
       ;
   }
 
+
   String fv = WiFi.firmwareVersion();
   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("WiFi module failed (WIFI_FIRMWARE_LATEST_VERSION)");
+    Serial.print("WiFi module failed (WIFI_FIRMWARE_LATEST_VERSION) board=");
+    Serial.print(fv);
+    Serial.print(" != ");
+    Serial.println(WIFI_FIRMWARE_LATEST_VERSION);
+
+    Serial.println(" >> HALTED <<");
+    while (true)
+      ;
   }
 
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
+
+
+
+  while (true) {
     Serial.print("Connecting to: ");
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
+    if (status == WL_CONNECTED) break;
     delay(10000);
   }
+
   Serial.println("Connected");
 
   IPAddress ip = WiFi.localIP();
@@ -90,7 +115,7 @@ void setup() {
   Serial.println(" dBm");
 
 
-  Serial.print("\nListening on port");
+  Serial.print("\nListening on port ");
   Serial.println(localPort);
   Udp.begin(localPort);
 }
@@ -99,6 +124,11 @@ void loop() {
 
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
+  if (packetSize == 0) {
+    delay(500);
+    return;
+  }
+  Udp.read((char *)&packetBuffer, sizeof(Packet));
   if (packetSize != sizeof(Packet)) {
     Serial.print("Invalid packet size ");
     Serial.print(packetSize);
@@ -106,9 +136,8 @@ void loop() {
     Serial.println(sizeof(Packet));
     return;
   }
-  Udp.read((char *)&packetBuffer, sizeof(Packet));
   if (packetBuffer.magic != PACKET_MAGIC) {
-    Serial.print("Invalid value for magic");
+    Serial.print("Invalid value for magic ");
     Serial.print(packetBuffer.magic, HEX);
     Serial.print(" != ");
     Serial.println(PACKET_MAGIC, HEX);
