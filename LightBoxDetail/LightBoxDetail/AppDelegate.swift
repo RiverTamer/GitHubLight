9cec2d0bd537eb5fd3b0e053f52938074a40d54d
Karl Kraft <karl@kraft.com>
2024-01-24 15:32:29 -0600

    Swift client for light box status

---
+++ LightBoxDetail/LightBoxDetail/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  LightBoxDetail
+//
+//  Created by Karl Kraft on 1/24/24.
+//  Copyright 2024 Karl Kraft. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
